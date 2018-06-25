a53f8406c8e381b288c4c426f26d55d1b8a0baf8
nikoloz110 <asatiani.nikoloz@gmail.com>
2018-06-25 20:07:59 +0400

    wdwdw

--- homeworks/24_june/car.js
+++ homeworks/24_june/car.js
@@ -1,14 +1,17 @@
 function Car() {
   this.setName =  function(nameInp) {
     this.name = nameInp;
+    return this;
   }
 
   this.setColor = function(colorInp){
     this.color = colorInp;
+    return this;
   }
     
   this.setBrand =  function(brandInp){
     this.brand = brandInp;
+    return this;
   }
   this.save = function() {
     console.log(`saving ${this.name}, color - ${this.color}, brand - ${this.brand}`)
